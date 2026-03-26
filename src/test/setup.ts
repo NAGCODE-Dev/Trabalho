3f0d51e73b1ad828143fb77a02231e94637ec0e8
NAGCODE-Dev <nagcode.contact@gmail.com>
2026-03-25 22:51:09 -0300

    `fix: type react act environment flag in test setup for production build`

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,4 +1,4 @@
 import '@testing-library/jest-dom'
 import 'fake-indexeddb/auto'
 
-globalThis.IS_REACT_ACT_ENVIRONMENT = true
+;(globalThis as typeof globalThis & { IS_REACT_ACT_ENVIRONMENT?: boolean }).IS_REACT_ACT_ENVIRONMENT = true
